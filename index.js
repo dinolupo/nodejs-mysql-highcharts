dcb7bf8e6103de14353e1a57548406efc6a3178d
Dino Lupo <dino.lupo@gmail.com>
2016-02-03 17:50:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var express   =    require("express");
+var mysql     =    require('mysql');
+var app       =    express();
+
+var pool      =    mysql.createPool({
+    connectionLimit : 100, //important
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'opc',
+    debug    :  false
+});
+
+app.use(express.static('public'));
+
+function handle_database(req,res) {
+    
+    pool.getConnection(function(err,connection){
+        if (err) {
+          connection.release();
+          res.json({"code" : 100, "status" : "Error in connection database"});
+          return;
+        }   
+
+        console.log('connected as id ' + connection.threadId);
+        
+		connection.query('SELECT * from opc.variables order by time_stamp', function(err, rows, fields) {
+            connection.release();
+            if(!err) {
+                res.json(rows);
+            }           
+        });
+
+        connection.on('error', function(err) {      
+              res.json({"code" : 100, "status" : "Error in connection database"});
+              return;     
+        });
+  });
+}
+
+app.get("/service1",function(req,res){
+        handle_database(req,res);
+});
+
+app.listen(3000);
+
